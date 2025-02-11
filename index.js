a6460764e775eaf6b094d207d24cf291560ce8c3
Alexander Polanco <alexander.polanco@gmail.com>
2025-02-10 21:55:46 -0300

    Se realizan los pasos del 3.13 al 3.14

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const PhoneBook = require("./models/phonebook");
 const app = express();
 
 app.use(cors());
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
 app.use(express.json());
 morgan.token("body", function (req, res) {
@@ -14,33 +15,6 @@ morgan.token("body", function (req, res) {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-let phonebook = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -56,38 +30,45 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(phonebook);
+  PhoneBook.find({}).then((result) => {
+    response.json(result);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const person = phonebook.find((person) => person.id === Number(id));
-  if (!person) {
-    response.status(404).send("Person not found");
-    return;
-  }
-  response.json(person);
+  PhoneBook.find({ _id: id })
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => {
+      response.status(404).send("Person not found");
+    });
 });
 
 app.post("/api/persons", (request, response) => {
-  const person = request.body;
-  const id = getRandomInt(1000);
+  const requestPerson = request.body;
   const newPerson = {
-    id: id,
-    name: person.name,
-    number: person.number,
+    name: requestPerson.name,
+    number: requestPerson.number,
   };
 
-  if ("name" in person === false) {
+  if ("name" in requestPerson === false) {
     response.status(400).json({ error: "name is required" });
     return;
   }
 
-  if ("number" in person === false) {
+  if ("number" in requestPerson === false) {
     response.status(400).json({ error: "number is required" });
     return;
   }
 
+  const person = new PhoneBook({ ...newPerson });
+  person.save().then((result) => {
+    response.json(result);
+  });
+
+  /*
   const isPerson = phonebook.find(
     (person) => person.name.toLowerCase() === newPerson.name.toLowerCase()
   );
@@ -96,16 +77,20 @@ app.post("/api/persons", (request, response) => {
     response.status(400).json({ error: "the person's name already exists" });
     return;
   }
-  response.json(newPerson);
+
+  */
+  //response.json(newPerson);
 });
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const person = phonebook.find((person) => person.id === Number(id));
-  if (!person) {
-    response.status(404).send("Person not found");
-    return;
-  }
+  PhoneBook.find({ _id: id })
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((error) => {
+      response.status(404).send("Person not found");
+    });
   response.status(204).json(person);
 });
 
---
+++ models/phonebook.js
@@ -0,0 +1,16 @@
+const mongoose = require("../conexion");
+
+const phoneBookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+phoneBookSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('PhoneBook', phoneBookSchema)
